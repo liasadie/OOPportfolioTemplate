f0c96957c445f2090cb91e8e193d7ee4b7984f99
Lia Cartwright <l.cartwright2564@student.leedsbeckett.ac.uk>
2024-02-06 14:24:53 +0000

    Created DIamond.java, Hello,java, PersonalDetails.java, nd Test.java

---
+++ WeeklyExercises/1GettingStarted/PersonalDetails.java
@@ -0,0 +1,10 @@
+public class PersonalDetails
+{
+    public static void main (String[] args)
+    {
+        System.out.println ("Name:");
+        System.out.println ("Address:");
+        System.out.println ("AgeL");
+        System.out.println ("Phonr number:");
+    }
+}
